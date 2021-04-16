0a784f581f073f3bdab046c3b1d713bb3ab64828
Elektrochecker <50339852+Elektrochecker@users.noreply.github.com>
2021-04-17 00:59:30 +0200

    vector visualization

--- java.js
+++ java.js
@@ -1,16 +1,27 @@
-let l = 400;
+const l = 400;
+const scl = 8;
 let axisLength = 2 * l - Math.sqrt(2 * l ** 2);
+let origin;
 
 function setup() {
   createCanvas(l, l);
-
+  origin = new Vector()
 }
 
 function draw() {
   background(200)
   translate(l - axisLength, axisLength)
   stroke(0)
-  line(0,0, -l, l) //x axis
-  line(0,0, 0, -l) //y axis
-  line(0,0, l, 0) //z axis
+  line(0, 0, -l + axisLength, l - axisLength) //x axis
+  line(0, 0, 0, -axisLength) //y axis
+  line(0, 0, axisLength, 0) //z axis
+
+  let a = new Vector(2, 0, 0)
+  let b = new Vector(0, 3, 0)
+
+  a.show(origin, [255,0,0])
+  b.show(origin, [0,255,0])
+
+  vectorProduct(a, b)
+    .show(origin, [0, 0, 255])
 }
--- vector.js
+++ vector.js
@@ -1,9 +1,24 @@
-function Vector(a,b,c) {
+function Vector(a=0,b=0,c=0) {
     this.x = a;
     this.y = b;
     this.z = c;
 
     this.abs = () => sqrt(this.x**2 + this.y**2 + this.z**2);
+
+    this.show = (start= new Vector(), color= [255,0,0])  => {
+        let startX = -start.x*sqrt(2)/2 + start.y;
+            startX *= scl
+        let startY = start.x*sqrt(2)/2 - start.z;
+            startY *= scl
+
+        let x = -this.x*sqrt(2)/2 + this.y;
+            x *= scl
+        let y = this.x*sqrt(2)/2 - this.z;
+            y *= scl
+        stroke(color)
+        line(startX,startY, x+startX,y+startY)
+        return this;
+    }
 }
 
 let vectorProduct = (a,b) => {
